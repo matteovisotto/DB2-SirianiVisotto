972e53513c7317fc580b51967efc299c61ed7912
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-03-24 17:50:24 +0100

    Add package post

--- Telco/src/main/java/it/polimi/db2/telco/servlet/AdminsServicePackageServlet.java
+++ Telco/src/main/java/it/polimi/db2/telco/servlet/AdminsServicePackageServlet.java
@@ -1,8 +1,14 @@
 package it.polimi.db2.telco.servlet;
 
+import it.polimi.db2.telco.controllers.OptionalProductController;
+import it.polimi.db2.telco.controllers.ServiceController;
 import it.polimi.db2.telco.controllers.ServicePackageController;
+import it.polimi.db2.telco.entities.OptionalProduct;
 import it.polimi.db2.telco.entities.PackagePrice;
+import it.polimi.db2.telco.entities.Service;
 import it.polimi.db2.telco.entities.ServicePackage;
+import it.polimi.db2.telco.exceptions.optionalProduct.OptionalProductException;
+import it.polimi.db2.telco.exceptions.service.ServiceException;
 import it.polimi.db2.telco.exceptions.servicePackage.ServicePackageException;
 
 import javax.inject.Inject;
@@ -21,6 +27,12 @@ public class AdminsServicePackageServlet extends HttpServlet {
     @Inject
     ServicePackageController servicePackageController;
 
+    @Inject
+    ServiceController serviceController;
+
+    @Inject
+    OptionalProductController optionalProductController;
+
     @Override
     public void init() throws ServletException {}
 
@@ -33,28 +45,60 @@ public class AdminsServicePackageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("p_name");
         String[] services = req.getParameterValues("p_services");
-        if (services == null) {
-            services = new String[]{};
+        if (services == null || services.length == 0) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "At least one service must be selected");
+            return;
         }
-        List<Integer> serviceIds = new ArrayList<>();
-        for (int i = 0; i < services.length; i++) {
-            serviceIds.add(Integer.parseInt(services[i]));
+        List<Service> selectedServices = new ArrayList<>();
+        try {
+            for (int i = 0; i < services.length; i++) {
+                Service service = serviceController.getServiceById(Integer.parseInt(services[i]));
+                selectedServices.add(service);
+            }
+        } catch (NumberFormatException e) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid data format");
+            return;
+        } catch (ServiceException s){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid data");
+            return;
         }
+
         ServicePackage servicePackage = new ServicePackage();
         servicePackage.setName(name);
-        //servicePackage.setServices(serviceIds);
+        servicePackage.setServices(selectedServices);
+
         String[] optionalIdsStr = req.getParameterValues("p_optionals");
         if (optionalIdsStr == null) {
             optionalIdsStr = new String[]{};
         }
-        List<Integer> optionalsIds = new ArrayList<>();
-        for (int i = 0; i < optionalIdsStr.length; i++) {
-            optionalsIds.add(Integer.parseInt(optionalIdsStr[i]));
+        List<OptionalProduct> selectedOptionals = new ArrayList<>();
+        try {
+            for (int i = 0; i < optionalIdsStr.length; i++) {
+                OptionalProduct optionalProduct = optionalProductController.getOptionalProductById(Integer.parseInt(optionalIdsStr[i]));
+                selectedOptionals.add(optionalProduct);
+            }
+        } catch (NumberFormatException e){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid data format");
+            return;
+        } catch (OptionalProductException o){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid data");
+            return;
         }
-        //servicePackage.setOptionalProducts(optionalIdsStr);
-        Double twelveMonth = Double.parseDouble(req.getParameter("p_p_12"));
-        Double twentyFourMonth = Double.parseDouble(req.getParameter("p_p_24"));
-        Double thirtySixMonth = Double.parseDouble(req.getParameter("p_p_36"));
+        servicePackage.setOptionalProducts(selectedOptionals);
+
+        Double twelveMonth = 0.0;
+        Double twentyFourMonth = 0.0;
+        Double thirtySixMonth = 0.0;
+
+        try {
+            twelveMonth = Double.parseDouble(req.getParameter("p_p_12"));
+            twentyFourMonth = Double.parseDouble(req.getParameter("p_p_24"));
+            thirtySixMonth = Double.parseDouble(req.getParameter("p_p_36"));
+        } catch (NumberFormatException e){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid data format");
+            return;
+        }
+
         PackagePrice twelve = new PackagePrice();
         twelve.setPrice(twelveMonth);
         twelve.setValidityPeriod(12);
